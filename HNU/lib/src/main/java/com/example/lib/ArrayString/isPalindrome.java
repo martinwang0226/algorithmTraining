06d9ff9ad87d93bdf840d2453c8134e647038352
habo <mc3895q@dingtalk.com>
2020-03-22 10:34:23 +0800

    复习，算法简单题目

--- HNU/lib/src/main/java/com/example/lib/ArrayString/isPalindrome.java
+++ HNU/lib/src/main/java/com/example/lib/ArrayString/isPalindrome.java
@@ -5,6 +5,8 @@ package com.example.lib.ArrayString;
  *
  * @author habo
  * @date 2020/3/21
+ * <p>
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
  */
 public class isPalindrome {
 
@@ -28,6 +30,12 @@ public class isPalindrome {
     }
 
     // (满足Follow up)：时间复杂度: O(1) 空间复杂度: O(1)
+//    刚才我们在思路 1 中通过将整数转为字符串处理，现在我们不转了，直接用整数类型来判断是否是回文数。
+// 首先负数肯定不是回文数，这个不用多说。其次就是如果一个数字为正整数，而且能够被 10 整除，那么这个数字也不是回文数，因为回文数的首位肯定不是 0 。
+//
+//    那么不把整数转为字符串该怎么做呢？还记得在 2.2 小节整数反转么？
+//
+//    没错，我们直接把整数反转过来，与原来的值比较即可。如果印象不深刻的同学可以去重新看下2.2小节。
 
     public boolean isPalindrome2(int x) {
         if (x < 0) { // 负数肯定不是palindrome
@@ -36,11 +44,11 @@ public class isPalindrome {
         int temp = x;
         // 翻转之后的数字可能超过整型的范围
         long y = 0;
-        while ( x != 0) {
+        while (x != 0) {
             y = y * 10 + x % 10;
             x /= 10;
         }
-        return  temp == y;
+        return temp == y;
     }
 
 }
--- HNU/lib/src/main/java/com/example/lib/ArrayString/reverseNum.java
+++ HNU/lib/src/main/java/com/example/lib/ArrayString/reverseNum.java
@@ -5,12 +5,17 @@ package com.example.lib.ArrayString;
  *
  * @author habo
  * @date 2020/3/21
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
  */
 public class reverseNum {
     public static void main(String[] args) {
         System.out.print("链表中的两数相加" + "=======================");
     }
 
+    //我们int型的数值范围是 -2147483648～2147483647 (-2^31 ~ 2^31 - 1)，
+    // 那么如果我们要翻转 1000000009 这个在数值范围内的数，得到 9000000001，但翻转后的数就超过了范围，
+    // 这个情况就是溢出，这个时候程序返回 0 即可。
+    //如果输入的是负数，就递归调用原函数，参数变成 -x 即可，代码大致执行流程如下
     public int reverse(int x) {
         if (x == -2147483648) { //如果不做这个判断，下面的x=-x将会报错
             return 0;
