5d9ad23481432702e941bfe0f52f19197306eba1
habo <mc3895q@dingtalk.com>
2020-03-22 14:15:50 +0800

    复习，算法简单题目3

--- HNU/lib/src/main/java/com/example/lib/linkedList/mergeTwoLists.java
+++ HNU/lib/src/main/java/com/example/lib/linkedList/mergeTwoLists.java
@@ -5,14 +5,19 @@ package com.example.lib.linkedList;
  *
  * @author habo
  * @date 2020/3/21
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 示例:
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
  */
 public class mergeTwoLists {
     public static void main(String[] args) {
-        System.out.print("链表中的两数相加" + "=======================");
+        System.out.print("两个链表进行合并" + "=======================");
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
+        //dummy链表
         ListNode prev = result;
         // 遍历两个链表，每次比较链表头的大小，每次让较小值添加到 dummy 的后面，并且让较小值所在的链表后移一位
         while (l1!= null && l2!= null) {
