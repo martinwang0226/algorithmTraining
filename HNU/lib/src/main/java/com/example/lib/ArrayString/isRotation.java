75fcfbb0da3881c8d89de8b85f9fa24a27bd11d3
martinwang <chenlong.wang@zhangmen.com>
2020-09-03 15:12:22 +0800

    [feature]算法9.3字符串和数组

---
+++ HNU/lib/src/main/java/com/example/lib/ArrayString/isRotation.java
@@ -0,0 +1,11 @@
+package com.example.lib.ArrayString;
+
+/**
+ *
+ * Create by chenlong.wang
+ * on 2020/9/2
+ 判断一个字符串是否为另一个字符串旋转而成
+ */
+public class isRotation {
+
+}
---
+++ HNU/lib/src/main/java/com/example/lib/treeGraph/inorderSucc.java
@@ -0,0 +1,15 @@
+package com.example.lib.treeGraph;
+
+/**
+ * Create by chenlong.wang
+ * on 2020/9/2
+ * 4.6　设计一个算法，找出二叉查找树中指定结点的“下一个”结点（也即中序后继）。可以假定每个结点都含有指向父结点的连接。（第54页）
+ *
+ * Gayle Laakmann McDowell. 程序员面试金典（第5版） (p. 154). 人民邮电出版社. Kindle 版本.
+ */
+public class inorderSucc {
+    public static void main(String[] args) {
+
+    }
+
+}
